5119b6d5377273040c4f120d5f90b5fbd8eefb7f
JONAHBRUZZI <jon.guerra@duocuc.cl>
2025-06-12 16:30:49 -0400

    configuracion de pnpm , client router y rutas dinamicas de especialidades

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
-
 import { defineConfig } from 'astro/config';
 import vercelAdapter from '@astrojs/vercel/serverless';
 import tailwindcss from '@tailwindcss/vite';
+
+
+import mdx from '@astrojs/mdx';
 
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   output: 'server',
   adapter: vercelAdapter({}),
-
+  integrations: [mdx()],
 });
