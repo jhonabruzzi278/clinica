2f1404d908fff34cd4e556d32c602338adc2423b
JONAHBRUZZI <jon.guerra@duocuc.cl>
2025-06-22 14:39:53 -0400

    verifiacion 1.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-import clerk from '@clerk/astro';
-import node from '@astrojs/node';
+
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [
-    clerk({
-      afterSignInUrl: '/',
-      afterSignUpUrl: '/',
-      ssoCallbackUrl: '/login/sso-callback',
-    })
-  ],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  
 });
